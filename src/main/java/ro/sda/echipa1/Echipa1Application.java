f287287a8ed0be48ed7c534fb3a08b56bbfe2e2d
stefan1202 <stefan.durla@gmail.com>
2021-10-17 13:04:52 +0300

    Init project

---
+++ src/main/java/ro/sda/echipa1/Echipa1Application.java
@@ -0,0 +1,13 @@
+package ro.sda.echipa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Echipa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Echipa1Application.class, args);
+	}
+
+}
