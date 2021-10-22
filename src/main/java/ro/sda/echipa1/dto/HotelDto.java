79b37f614ed9fd310716583a55af69d1ec8ba272
Lavinia <laviniaemanuela1988@gmail.com>
2021-10-22 19:26:30 +0300

    html

---
+++ src/main/java/ro/sda/echipa1/dto/HotelDto.java
@@ -0,0 +1,45 @@
+package ro.sda.echipa1.dto;
+
+import ro.sda.echipa1.entities.City;
+import ro.sda.echipa1.entities.StarRating;
+
+public class HotelDto {
+
+    private String name;
+    private String description;
+    private StarRating starRating;
+    private City city;
+
+    public StarRating getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(StarRating starRating) {
+        this.starRating = starRating;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
