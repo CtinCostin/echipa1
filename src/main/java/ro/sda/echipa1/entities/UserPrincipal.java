d940b97d0ca83f3bdbe07eb3897576b1e3fe2193
Costi <costi_georgescu2000@yahho.com>
2021-10-26 20:36:31 +0300

    more controllers made

---
+++ src/main/java/ro/sda/echipa1/entities/UserPrincipal.java
@@ -0,0 +1,62 @@
+package ro.sda.echipa1.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        user.getRolesList()
+                .forEach(role -> {
+                    GrantedAuthority authority = new SimpleGrantedAuthority("ROLE" + role);
+                    authorities.add(authority);
+                });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
